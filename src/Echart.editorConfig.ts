aec9eca54c9a39a9827a5f04ffd3c61a95e0e013
engalar <engalar@gmail.com>
2022-05-16 14:26:59 +0800

    rename

--- src/Echart.editorConfig.ts
+++ src/Echart.editorConfig.ts
@@ -1,8 +1,8 @@
 import { Properties, StructurePreviewProps, transformGroupsIntoTabs } from "./piw-utils-internal";
-import { GraphPreviewProps } from "../typings/GraphProps";
+import { EchartPreviewProps } from "../typings/EchartProps";
 
 export function getProperties(
-    values: GraphPreviewProps,
+    values: EchartPreviewProps,
     defaultProperties: Properties,
     platform: "web" | "desktop"
 ): Properties {
@@ -12,7 +12,7 @@ export function getProperties(
     }
     return defaultProperties;
 }
-export function getPreview(values: GraphPreviewProps): StructurePreviewProps | null {
+export function getPreview(values: EchartPreviewProps): StructurePreviewProps | null {
     console.log(values);
     return null;
 }
--- src/Echart.editorPreview.tsx
+++ src/Echart.editorPreview.tsx
@@ -1,10 +1,10 @@
 import { parseStyle } from "./piw-utils-internal";
 import { createElement } from "react";
-import { GraphPreviewProps } from "../typings/GraphProps";
+import { EchartPreviewProps } from "../typings/EchartProps";
 
 declare function require(name: string): string;
 
-export function preview(props: GraphPreviewProps) {
+export function preview(props: EchartPreviewProps) {
     return <div style={parseStyle(props.style)}></div>;
 }
 
